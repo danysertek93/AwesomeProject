cb2f0b9efa33cae385a02f5257900e42346744e6
danyUr <dany9303@hotmail.es>
2020-03-09 16:15:35 -0600

    commit-app-intro-skip

--- App.js
+++ App.js
@@ -1,62 +1,132 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-
-import AppNavigator from './navigation/AppNavigator';
-
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Platform } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import Main from './components/Main'
+ 
+export default class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+ 
+      show_Main_App: false
+ 
+    };
+  }
+ 
+  on_Done_all_slides = () => {
+    this.setState({ show_Main_App: true });
+  };
+ 
+  on_Skip_slides = () => {
+    this.setState({ show_Main_App: true });
+  };
+  render() {
+    if (this.state.show_Main_App) {
+      return (
+        <Main/>
+      );
+    } else {
+      return (
+        <AppIntroSlider
+          slides={slides}
+          onDone={this.on_Done_all_slides}
+          showSkipButton={true}
+          onSkip={this.on_Skip_slides}
+        />
+      );
+    }
   }
 }
-
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
 const styles = StyleSheet.create({
-  container: {
+ 
+  MainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
   },
+  title: {
+    fontSize: 26,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain'
+  }
 });
+ 
+const slides = [
+  {
+    key: 'k1',
+    title: 'Event Organizer',
+    text: 'Best Event Organizers',
+    image: {
+      uri:
+        'https://reactnativecode.com/wp-content/uploads/2019/04/calendar.png',
+    },
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#FF1744',
+  },
+  {
+    key: 'k2',
+    title: 'Weather Reports',
+    text: 'Latest Weather Reports',
+    image: {
+      uri:
+        'https://reactnativecode.com/wp-content/uploads/2019/04/cloud.png',
+    },
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#D500F9',
+  },
+  {
+    key: 'k3',
+    title: 'Technology Informations',
+    text: 'Latest Technology Reports',
+    image: {
+      uri: 'https://reactnativecode.com/wp-content/uploads/2019/04/computer.png',
+    },
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#2979FF',
+  },
+  {
+    key: 'k4',
+    title: 'Flight Bookings',
+    text: ' Best Deals on Flights',
+    image: {
+      uri: 'https://reactnativecode.com/wp-content/uploads/2019/04/flight.png',
+    },
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#1DE9B6',
+  },
+  {
+    key: 'k5',
+    title: 'Restaurant Bookings',
+    text: ' 20% off on first Restaurant booking',
+    image: {
+      uri:
+        'https://reactnativecode.com/wp-content/uploads/2019/04/restaurants.png',
+    },
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#FF3D00',
+  },
+];
